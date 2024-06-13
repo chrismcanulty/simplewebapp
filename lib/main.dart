f789dbab05116e4e4e2152296f0d8752f9855e76
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2024-06-13 15:40:54 -0700

    cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -150,8 +150,6 @@ Widget buildRobots(List<Robot> robots) {
         width: double.maxFinite,
         child: Row(
           children: [
-            Expanded(flex: 1, child: Image.network(robot.title!)),
-            const SizedBox(width: 10),
             Expanded(flex: 3, child: Text(robot.title!)),
           ],
         ),
@@ -191,34 +189,4 @@ class Robot {
     id = json['id'];
     title = json['title'];
   }
-
-  // Map<String, dynamic> toJson() {
-  //   final Map<String, dynamic> data = <String, dynamic>{};
-  //   data['userId'] = userId;
-  //   data['id'] = id;
-  //   data['title'] = title;
-  //   return data;
-  // }
 }
-
-// class RobotList {
-//   int? userId;
-//   int? id;
-//   String? title;
-
-//   RobotList({this.userId, this.id, this.title});
-
-//   RobotList.fromJson(Map<String, dynamic> json) {
-//     userId = json['userId'];
-//     id = json['id'];
-//     title = json['title'];
-//   }
-
-//   Map<String, dynamic> toJson() {
-//     final Map<String, dynamic> data = <String, dynamic>{};
-//     data['userId'] = userId;
-//     data['id'] = id;
-//     data['title'] = title;
-//     return data;
-//   }
-// }
