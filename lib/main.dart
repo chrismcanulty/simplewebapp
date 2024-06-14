7b5cad1ce1fa379918415fb337d11bb94ac7dd91
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2024-06-13 18:08:14 -0700

    corrected classgit add -A

--- lib/main.dart
+++ lib/main.dart
@@ -95,12 +95,12 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late Future<List<Robot>> futureAlbum;
+  late final Future<List<Robot>> futureRobots;
 
   @override
   void initState() {
     super.initState();
-    futureAlbum = fetchAlbum();
+    futureRobots = fetchRobots();
   }
 
   @override
@@ -116,10 +116,10 @@ class _HomeScreenState extends State<HomeScreen> {
         ),
         body: Center(
           child: FutureBuilder<List<Robot>>(
-            future: futureAlbum,
-            builder: (context, snapshot) {
+            future: futureRobots,
+            builder: (context, AsyncSnapshot<List<Robot>> snapshot) {
               // ignore: avoid_print
-              print('snapshotdata: $snapshot.data');
+              print('snapshotdata!!: $snapshot.data');
               if (snapshot.hasData) {
                 final robots = snapshot.data;
                 return buildRobots(robots!);
@@ -150,7 +150,7 @@ Widget buildRobots(List<Robot> robots) {
         width: double.maxFinite,
         child: Row(
           children: [
-            Expanded(flex: 3, child: Text(robot.title!)),
+            Expanded(flex: 3, child: Text(robot.robotName)),
           ],
         ),
       );
@@ -158,8 +158,8 @@ Widget buildRobots(List<Robot> robots) {
   );
 }
 
-Future<List<Robot>> fetchAlbum() async {
-  var url = Uri.parse('https://jsonplaceholder.typicode.com/albums');
+Future<List<Robot>> fetchRobots() async {
+  var url = Uri.parse('https://map-api.ik-robot.com/master/robot');
   final response =
       await http.get(url, headers: {"Content-Type": "application/json"});
 
@@ -173,20 +173,217 @@ Future<List<Robot>> fetchAlbum() async {
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
-    throw Exception('Failed to load album');
+    throw Exception('Failed to load');
   }
 }
+
+// Create class for individual Robot data
+
+List<Robot> robotFromJson(String str) =>
+    List<Robot>.from(json.decode(str).map((x) => Robot.fromJson(x)));
+
+String robotToJson(List<Robot> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
 
 class Robot {
-  int? userId;
-  int? id;
-  String? title;
+  List<MapInfo> mapInfo;
+  String? mapId;
+  int updatedAt;
+  int createdAt;
+  String? mapName;
+  String robotName;
+  String robotId;
 
-  Robot({this.userId, this.id, this.title});
+  Robot({
+    required this.mapInfo,
+    this.mapId,
+    required this.updatedAt,
+    required this.createdAt,
+    this.mapName,
+    required this.robotName,
+    required this.robotId,
+  });
 
-  Robot.fromJson(Map<String, dynamic> json) {
-    userId = json['userId'];
-    id = json['id'];
-    title = json['title'];
+  factory Robot.fromJson(Map<String, dynamic> json) => Robot(
+        mapInfo:
+            List<MapInfo>.from(json["mapInfo"].map((x) => MapInfo.fromJson(x))),
+        mapId: json["mapId"],
+        updatedAt: json["updatedAt"],
+        createdAt: json["createdAt"],
+        mapName: json["mapName"],
+        robotName: json["robotName"],
+        robotId: json["robotId"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "mapInfo": List<dynamic>.from(mapInfo.map((x) => x.toJson())),
+        "mapId": mapId,
+        "updatedAt": updatedAt,
+        "createdAt": createdAt,
+        "mapName": mapName,
+        "robotName": robotName,
+        "robotId": robotId,
+      };
+}
+
+class MapInfo {
+  List<String>? homePointList;
+  String mapId;
+  List<String>? localizePointList;
+  List<MapPointList> mapPointList;
+  String? mapName;
+
+  MapInfo({
+    this.homePointList,
+    required this.mapId,
+    this.localizePointList,
+    required this.mapPointList,
+    this.mapName,
+  });
+
+  factory MapInfo.fromJson(Map<String, dynamic> json) => MapInfo(
+        homePointList: json["homePointList"] == null
+            ? []
+            : List<String>.from(json["homePointList"]!.map((x) => x)),
+        mapId: json["mapId"],
+        localizePointList: json["localizePointList"] == null
+            ? []
+            : List<String>.from(json["localizePointList"]!.map((x) => x)),
+        mapPointList: List<MapPointList>.from(
+            json["mapPointList"].map((x) => MapPointList.fromJson(x))),
+        mapName: json["mapName"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "homePointList": homePointList == null
+            ? []
+            : List<dynamic>.from(homePointList!.map((x) => x)),
+        "mapId": mapId,
+        "localizePointList": localizePointList == null
+            ? []
+            : List<dynamic>.from(localizePointList!.map((x) => x)),
+        "mapPointList": List<dynamic>.from(mapPointList.map((x) => x.toJson())),
+        "mapName": mapName,
+      };
+}
+
+class MapPointList {
+  String mapId;
+  List<StationDeg> stationDeg;
+  double x;
+  double y;
+  Category category;
+  int createdAt;
+  String pointId;
+  String? areaId;
+  String id;
+  String? pointName;
+  List<String>? tags;
+  double? direction;
+
+  MapPointList({
+    required this.mapId,
+    required this.stationDeg,
+    required this.x,
+    required this.y,
+    required this.category,
+    required this.createdAt,
+    required this.pointId,
+    this.areaId,
+    required this.id,
+    this.pointName,
+    this.tags,
+    this.direction,
+  });
+
+  factory MapPointList.fromJson(Map<String, dynamic> json) => MapPointList(
+        mapId: json["mapId"],
+        stationDeg: List<StationDeg>.from(
+            json["stationDeg"].map((x) => StationDeg.fromJson(x))),
+        x: json["x"]?.toDouble(),
+        y: json["y"]?.toDouble(),
+        category: categoryValues.map[json["category"]]!,
+        createdAt: json["createdAt"],
+        pointId: json["pointId"],
+        areaId: json["areaId"],
+        id: json["id"],
+        pointName: json["pointName"],
+        tags: json["tags"] == null
+            ? []
+            : List<String>.from(json["tags"]!.map((x) => x)),
+        direction: json["direction"]?.toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "mapId": mapId,
+        "stationDeg": List<dynamic>.from(stationDeg.map((x) => x.toJson())),
+        "x": x,
+        "y": y,
+        "category": categoryValues.reverse[category],
+        "createdAt": createdAt,
+        "pointId": pointId,
+        "areaId": areaId,
+        "id": id,
+        "pointName": pointName,
+        "tags": tags == null ? [] : List<dynamic>.from(tags!.map((x) => x)),
+        "direction": direction,
+      };
+}
+
+enum Category { LOCALIZE, NONE, ROUTE }
+
+final categoryValues = EnumValues({
+  "LOCALIZE": Category.LOCALIZE,
+  "NONE": Category.NONE,
+  "ROUTE": Category.ROUTE
+});
+
+class StationDeg {
+  double rad;
+  String stationId;
+  String dstId;
+
+  StationDeg({
+    required this.rad,
+    required this.stationId,
+    required this.dstId,
+  });
+
+  factory StationDeg.fromJson(Map<String, dynamic> json) => StationDeg(
+        rad: json["rad"]?.toDouble(),
+        stationId: json["stationId"],
+        dstId: json["dstId"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "rad": rad,
+        "stationId": stationId,
+        "dstId": dstId,
+      };
+}
+
+class EnumValues<T> {
+  Map<String, T> map;
+  late Map<T, String> reverseMap;
+
+  EnumValues(this.map);
+
+  Map<T, String> get reverse {
+    reverseMap = map.map((k, v) => MapEntry(v, k));
+    return reverseMap;
   }
 }
+
+// class Robot {
+//   int? userId;
+//   int? id;
+//   String? title;
+
+//   Robot({this.userId, this.id, this.title});
+
+//   Robot.fromJson(Map<String, dynamic> json) {
+//     userId = json['userId'];
+//     id = json['id'];
+//     title = json['title'];
+//   }
+// }
